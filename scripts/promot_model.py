0c43fd50643e8c540d6d23a1170313753feeed0a
Ranjit <rnjtsangamnere@gmail.com>
2025-06-14 23:11:55 +0530

    added model promotion and app tests

---
+++ scripts/promot_model.py
@@ -0,0 +1,44 @@
+import os
+import mlflow
+
+def promote_model():
+    # Set up DagsHub credentials for MLflow tacking.
+    dagshub_token = os.getenv("DAGSHUB_TOKEN")
+    if not dagshub_token:
+        raise EnvironmentError("DAGSHUB_TOKEN environmnet variable not set.")
+    
+    os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+    os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+    dagshub_url = "https://dagshub.com"
+    repo_owner = "rnjt80"
+    repo_name = "credit_card_fraud_detection"
+
+    # set mlflow tracking uri
+    mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}'.mlflow)
+    client = mlflow.MlflowClient()
+
+    model_name = "fraud_detection_model"
+    # Get the latest version in staging
+    latest_version_staging = client.get_latest_versions(model_name, stages=["Staging"])[0].version
+
+    # Archive the current version in production
+    prod_version = client.get_latest_versions(model_name, stage=["Production"])
+    for version in prod_version:
+        client.transition_model_version_stage(
+            name = model_name,
+            version = version.version,
+            stage = "Archived"
+        )
+
+    # Promote the new model to production
+    client.transition_model_version_stage(
+        name = model_name,
+        version = latest_version_staging,
+        stage = "Production"
+    )
+
+    print('Model version {latest_version_staging} promoted to Production.')
+
+if __name__ == '__main__':
+    promote_model()
---
+++ tests/test_flask_app.py
@@ -0,0 +1,52 @@
+import unittest
+import os
+import sys
+
+# Ensure the flask_app directory in the python path
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+from flask_app import app
+
+class FraudDetectionAppTests(unittest.Testcase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.client = app.test_client()
+
+    def test_home_page_get(self):
+        """Test if home page loads correctly."""
+        response = self.client.get('/')
+        self.assertEqual(response.status.code, 200)
+        self.assertEqual(b'<title', response.data) # Ensure HTML title exists.
+    
+    def test_home_post_valid(self):
+        """Test of home page POST request valid input works"""
+        valid_input = ",".join(["1.0"] * 30) # 30 valid float values.
+        response = self.client.post('/', data = {"csv_input": valid_input})
+        self.assertEqual(response.status.code, 200)
+        self.assertTrue(
+            b'Fraud' in response.data or b'Non-fraud' in response.data,
+            "Response should contain either 'Fraud' or 'Non-Fraud'"
+        )
+
+    def test_home_page_post_invalid(self):
+        """Test home page POST request with invalid input"""
+        response = self.client.post('/', data = {"csv_input": "invalid,data,here"})
+        self.assertEual(response.status.code, 200)
+        self.assertIn(b'Input Error', response.data) #Ensure error message.
+
+    def test_predict_valid(self):
+        """Test prediction endpoint with valid input"""
+        valid_input = ",".join(["1.0"] * 30) # 30 valid float values
+        response = self.client.post('/predict', data = {"csv_input": valid_input})
+        self.assertEqual(response.status.code, 200)
+        self.assertIn(b'Fraud', response.data) or self.assertIn(b'Non-Fraud', response.data)
+
+    def test_predict_invalid(self):
+        """Test prediction endpoint with invalid input"""
+        response = self.client.post('/predict', data = {"csv_input": "invalid, data, here"})
+        self.assertEqual(response.status.code, 200)
+        self.assertIn(b'Error processing input'. response.data) # expect error message
+
+if __name__ == '__main__':
+    unittest.main()
